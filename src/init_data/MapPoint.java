aaedd69cb99aeb10f8ba7a33e983ae6212ce8e17
tienhai912 <tienhai912@yahoo.com.vn>
2017-06-01 14:04:04 +0700

    Add files via upload

---
+++ src/init_data/MapPoint.java
@@ -0,0 +1,21 @@
+package init_data;
+
+public class MapPoint {
+	private float x,y;
+
+	public MapPoint(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public float getY() {
+		return y;
+	}
+	
+	
+
+}
